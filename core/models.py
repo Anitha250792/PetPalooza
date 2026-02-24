709fd8fe308f1ad3640e4acba02530a01d4e8719
Anitha250792 <ntanithasaravanan@gmail.com>
2026-02-24 13:28:56 +0530

    Convert ImageField to CloudinaryField

--- core/models.py
+++ core/models.py
@@ -1,18 +1,19 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 
 class HeroSlide(models.Model):
-      title = models.CharField(max_length=200)
-      image = models.ImageField(upload_to='hero/')
-      is_active = models.BooleanField(default=True)
+    title = models.CharField(max_length=200)
+    image = CloudinaryField('image')
+    is_active = models.BooleanField(default=True)
 
-      def __str__(self):
+    def __str__(self):
         return self.title
 
 
 class PetCategory(models.Model):
     name = models.CharField(max_length=100)
-    image = models.ImageField(upload_to="pets/")
+    image = CloudinaryField('image')
     order = models.PositiveIntegerField(default=0)
 
     class Meta:
@@ -26,7 +27,7 @@ class PromoSection(models.Model):
     title = models.CharField(max_length=300)
     description = models.TextField(blank=True)
     button_text = models.CharField(max_length=100, blank=True)
-    image = models.ImageField(upload_to="promo/")
+    image = CloudinaryField('image')
 
     def __str__(self):
         return self.title
@@ -35,7 +36,7 @@ class PromoSection(models.Model):
 class ServiceCard(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    image = models.ImageField(upload_to="services/")
+    image = CloudinaryField('image')
     order = models.PositiveIntegerField(default=0)
 
     class Meta:
