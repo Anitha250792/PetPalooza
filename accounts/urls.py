ec8bf06601d7f99fdc85599f7e81629a7f4c1032
Anitha250792 <ntanithasaravanan@gmail.com>
2026-02-27 12:10:35 +0530

    Clean MySQL

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('search/', views.search, name='search'),
     path('petservice/', views.petservice_view, name='petservice'),
     path('contact/', views.contact_view, name='contact'),
-    path("check-db/", account_views.check_db),
+    path("check-db/", views.check_db),
     path('consult/', views.consult_view, name='consult'),
 ]
