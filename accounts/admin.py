5ab73e5d560064b449219c2c0a116abd07d4f64b
Anitha250792 <ntanithasaravanan@gmail.com>
2026-03-02 13:00:08 +0530

    accounts/admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,25 +1,15 @@
 from django.contrib import admin
 from .models import ContactMessage
-from django.core.mail import send_mail
-from django.conf import settings
 from django.utils import timezone
+from django.conf import settings
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 
 @admin.register(ContactMessage)
 class ContactMessageAdmin(admin.ModelAdmin):
 
-    list_display = (
-        "ticket_id",
-        "name",
-        "email",
-        "subject",
-        "status",
-        "created_at",
-    )
-
-    list_filter = ("status", "created_at")
-    search_fields = ("ticket_id", "name", "email", "subject")
-
+    list_display = ("ticket_id", "name", "email", "is_replied", "created_at")
     readonly_fields = (
         "ticket_id",
         "name",
@@ -36,22 +26,38 @@ class ContactMessageAdmin(admin.ModelAdmin):
         "subject",
         "message",
         "admin_reply",
-        "status",
+        "is_replied",
     )
 
     def save_model(self, request, obj, form, change):
 
-        # If admin wrote reply AND status is not already resolved
-        if obj.admin_reply and obj.status != "Resolved":
+        # Only send email if admin entered reply and not already replied
+        if obj.admin_reply and not obj.is_replied:
 
-            send_mail(
-                subject=f"Reply to your Ticket {obj.ticket_id}",
-                message=obj.admin_reply,
-                from_email=settings.DEFAULT_FROM_EMAIL,
-                recipient_list=[obj.email],
-                fail_silently=False,
-            )
+            try:
+                message = Mail(
+                    from_email=settings.DEFAULT_FROM_EMAIL,
+                    to_emails=obj.email,
+                    subject=f"Reply to your Ticket {obj.ticket_id}",
+                    html_content=f"""
+                        <h3>Petpalooza Support</h3>
+                        <p><strong>Your Query:</strong></p>
+                        <p>{obj.message}</p>
+                        <hr>
+                        <p><strong>Our Reply:</strong></p>
+                        <p>{obj.admin_reply}</p>
+                        <br>
+                        <p>Thank you for contacting us ❤️</p>
+                    """
+                )
 
-            obj.status = "Resolved"
+                sg = SendGridAPIClient(settings.SENDGRID_API_KEY)
+                sg.send(message)
+
+                obj.is_replied = True
+                obj.replied_at = timezone.now()
+
+            except Exception as e:
+                print("SendGrid Admin Reply Error:", e)
 
         super().save_model(request, obj, form, change)
