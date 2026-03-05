e412fe3cd887652d6867c904ec8c4a2511d0f429
Anitha250792 <ntanithasaravanan@gmail.com>
2026-03-05 11:26:57 +0530

    admin

--- cart/admin.py
+++ cart/admin.py
@@ -1,9 +1,47 @@
 from django.contrib import admin
-from .models import Product, Cart, CartItem, Service, OrderItem
+from .models import Product, Cart, CartItem, Service, OrderItem, Order
 
-admin.site.register(OrderItem)
 
 admin.site.register(Product)
 admin.site.register(Cart)
 admin.site.register(CartItem)
 admin.site.register(Service)
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+    readonly_fields = ("product", "service", "price", "quantity", "status")
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "id",
+        "user",
+        "name",
+        "email",
+        "total_amount",
+        "is_paid",
+        "created_at"
+    )
+
+    search_fields = ("email", "name")
+
+    list_filter = ("is_paid", "created_at")
+
+    inlines = [OrderItemInline]
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "order",
+        "product",
+        "service",
+        "quantity",
+        "price",
+        "status"
+    )
