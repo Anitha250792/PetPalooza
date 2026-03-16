95d3beb4bf8020950cafa57ee591e210a8b44bd9
Anitha250792 <ntanithasaravanan@gmail.com>
2026-03-16 17:11:10 +0530

    product describtion models

--- cart/models.py
+++ cart/models.py
@@ -9,7 +9,7 @@ class Product(models.Model):
 
     name = models.CharField(max_length=200)
 
-    description = models.TextField(blank=True, null=True)   # ⭐ ADD THIS
+    description = models.TextField(blank=True, null=True)
 
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
@@ -29,6 +29,7 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
 
 # ---------------- SERVICES ---------------- #
 
